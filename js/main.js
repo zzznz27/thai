d793bd729282fd3561bdbf35e9aa892b92c556d4
zach <zacherberstein@hotmail.com>
2020-01-24 16:51:16 +1100

    added animation V sick

--- js/main.js
+++ js/main.js
@@ -1,43 +1,51 @@
-function changeDish(){
+function changeDish() {
     var x = document.getElementById("dish");
 
-     if(x.value == "Noodle"){
+    if (x.value == "Noodle") {
         document.getElementById("noodle").style.display = "block";
         document.getElementById("curry").style.display = "none";
         document.getElementById("stirfry").style.display = "none";
-        document.getElementById("noodle").style.animationName ="wobble";
+        document.getElementById("noodle").style.animationName = "wobble";
         document.getElementById("noodle").style.animationDuration = "0.75s";
         document.getElementById("stirfry").disabled = "disabled";
         document.getElementById("noodle").disabled = "enabled";
         document.getElementById("curry").disabled = "disabled";
 
-     }
-     if(x.value == "Curry"){
+    }
+    if (x.value == "Curry") {
         document.getElementById("curry").style.display = "block";
         document.getElementById("stirfry").style.display = "none";
         document.getElementById("noodle").style.display = "none";
         document.getElementById("stirfry").disabled = "disabled";
         document.getElementById("noodle").disabled = "disabled";
         document.getElementById("curry").disabled = "enabled";
-     }
-     if(x.value == "StirFry"){
+        document.getElementById("curry").style.animationName = "rubberBand";
+        document.getElementById("curry").style.animationDuration = "0.75s";
+    }
+    if (x.value == "StirFry") {
         document.getElementById("curry").style.display = "none";
         document.getElementById("stirfry").style.display = "block";
         document.getElementById("noodle").style.display = "none";
         document.getElementById("stirfry").disabled = "enabled";
         document.getElementById("noodle").disabled = "disabled";
         document.getElementById("curry").disabled = "disabled";
-     }
- }
- function hideLists(){
-        document.getElementById("noodle").style.display = "none";
-        document.getElementById("curry").style.display = "none";
-        document.getElementById("stirfry").style.display = "none";
-        document.getElementById("stirfry").disabled = "disabled";
-        document.getElementById("noodle").disabled = "disabled";
-        document.getElementById("curry").disabled = "disabled";
-     
- }
+        document.getElementById("stirfry").style.animationName = "rollIn";
+        document.getElementById("stirfry").style.animationDuration = "0.75s";
+    }
+}
+
+function onLoad() {
+    document.getElementById("formContainer").style.display = "block";
+    document.getElementById("formContainer").style.animationName = "fadeInDown";
+    document.getElementById("formContainer").style.animationDuration = "3s";
+    document.getElementById("noodle").style.display = "none";
+    document.getElementById("curry").style.display = "none";
+    document.getElementById("stirfry").style.display = "none";
+    document.getElementById("stirfry").disabled = "disabled";
+    document.getElementById("noodle").disabled = "disabled";
+    document.getElementById("curry").disabled = "disabled";
+
+}
 
 (function ($) {
     "use strict";
@@ -45,19 +53,18 @@ function changeDish(){
 
     /*==================================================================
     [ Focus Contact2 ]*/
-    $('.input100').each(function(){
-        $(this).on('blur', function(){
-            if($(this).val().trim() != "") {
+    $('.input100').each(function () {
+        $(this).on('blur', function () {
+            if ($(this).val().trim() != "") {
                 $(this).addClass('has-val');
-            }
-            else {
+            } else {
                 $(this).removeClass('has-val');
             }
-        })    
+        })
     })
 
-    
-  
+
+
     /*==================================================================
     [ Validate ]*/
     var name = $('.validate-input input[name="name"]');
@@ -66,25 +73,25 @@ function changeDish(){
     var drink = $('.validate-input select[name="drink"]');
 
 
-    $('.validate-form').on('submit',function(){
+    $('.validate-form').on('submit', function () {
         var check = true;
 
-        if($(name).val().trim() == ''){
+        if ($(name).val().trim() == '') {
             showValidate(name);
-            check=false;
+            check = false;
         }
-        if($(drink).val().trim() == "Select Option"){
+        if ($(drink).val().trim() == "Select Option") {
             showValidate(drink);
-            check=false;
+            check = false;
         }
         return check;
     });
 
 
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
-       });
+    $('.validate-form .input100').each(function () {
+        $(this).focus(function () {
+            hideValidate(this);
+        });
     });
 
 
@@ -99,8 +106,8 @@ function changeDish(){
 
         $(thisAlert).removeClass('alert-validate');
     }
-    
-    
-    
+
+
+
 
 })(jQuery);
