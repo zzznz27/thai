2a4e41b2e5d274daa79d8582078d2d2d2f4aa953
Ryan Harper <ryanharper10@gmail.com>
2020-01-24 16:19:44 +1100

    dan

---
+++ js/main.js
@@ -0,0 +1,106 @@
+function changeDish(){
+    var x = document.getElementById("dish");
+
+     if(x.value == "Noodle"){
+        document.getElementById("noodle").style.display = "block";
+        document.getElementById("curry").style.display = "none";
+        document.getElementById("stirfry").style.display = "none";
+        document.getElementById("noodle").style.animationName ="wobble";
+        document.getElementById("noodle").style.animationDuration = "0.75s";
+        document.getElementById("stirfry").disabled = "disabled";
+        document.getElementById("noodle").disabled = "enabled";
+        document.getElementById("curry").disabled = "disabled";
+
+     }
+     if(x.value == "Curry"){
+        document.getElementById("curry").style.display = "block";
+        document.getElementById("stirfry").style.display = "none";
+        document.getElementById("noodle").style.display = "none";
+        document.getElementById("stirfry").disabled = "disabled";
+        document.getElementById("noodle").disabled = "disabled";
+        document.getElementById("curry").disabled = "enabled";
+     }
+     if(x.value == "StirFry"){
+        document.getElementById("curry").style.display = "none";
+        document.getElementById("stirfry").style.display = "block";
+        document.getElementById("noodle").style.display = "none";
+        document.getElementById("stirfry").disabled = "enabled";
+        document.getElementById("noodle").disabled = "disabled";
+        document.getElementById("curry").disabled = "disabled";
+     }
+ }
+ function hideLists(){
+        document.getElementById("noodle").style.display = "none";
+        document.getElementById("curry").style.display = "none";
+        document.getElementById("stirfry").style.display = "none";
+        document.getElementById("stirfry").disabled = "disabled";
+        document.getElementById("noodle").disabled = "disabled";
+        document.getElementById("curry").disabled = "disabled";
+     
+ }
+
+(function ($) {
+    "use strict";
+
+
+    /*==================================================================
+    [ Focus Contact2 ]*/
+    $('.input100').each(function(){
+        $(this).on('blur', function(){
+            if($(this).val().trim() != "") {
+                $(this).addClass('has-val');
+            }
+            else {
+                $(this).removeClass('has-val');
+            }
+        })    
+    })
+
+    
+  
+    /*==================================================================
+    [ Validate ]*/
+    var name = $('.validate-input input[name="name"]');
+    var email = $('.validate-input input[name="email"]');
+    var message = $('.validate-input textarea[name="message"]');
+    var drink = $('.validate-input select[name="drink"]');
+
+
+    $('.validate-form').on('submit',function(){
+        var check = true;
+
+        if($(name).val().trim() == ''){
+            showValidate(name);
+            check=false;
+        }
+        if($(drink).val().trim() == "Select Option"){
+            showValidate(drink);
+            check=false;
+        }
+        return check;
+    });
+
+
+    $('.validate-form .input100').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+       });
+    });
+
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+    
+    
+    
+
+})(jQuery);
---
+++ server.js
@@ -0,0 +1,50 @@
+var http = require('http');
+var connect = require('connect');
+var serveStatic = require('serve-static');
+var express = require('express')
+var fs = require('fs')
+var app = express()
+var path = require('path');
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/'));
+
+app.post('/order', function (req, res) {
+    let date_ob = new Date();
+    let date = ("0" + date_ob.getDate()).slice(-2);
+    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+    let year = date_ob.getFullYear();
+    // current hours
+    let hours = date_ob.getHours();
+    // current minutes
+    let minutes = date_ob.getMinutes();
+    // current seconds
+    let seconds = date_ob.getSeconds();
+
+
+    console.log('Order Recieved')
+    console.log( hours + ":" + minutes + ":" + seconds  + "  " + date + "-" + month + "-" +  year);
+    console.log(req.body)
+
+
+    /////////
+    res.writeHead(301, { Location: '/OrderPlaced.html' });
+    res.end();
+    return (res)
+});
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(8080, function () {
+    console.log('Server running on 8080...');
+});
+
